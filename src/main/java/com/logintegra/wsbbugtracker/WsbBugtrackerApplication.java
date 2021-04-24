5dfeb8ba2e0561fa8a116e632747762139af6e82
Katarzyna Duchowska <k.duchowska@o2.pl>
2021-04-24 15:56:40 +0200

    base

---
+++ src/main/java/com/logintegra/wsbbugtracker/WsbBugtrackerApplication.java
@@ -0,0 +1,14 @@
+package com.logintegra.wsbbugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WsbBugtrackerApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbBugtrackerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/logintegra/wsbbugtracker/issues/Issue.java
@@ -0,0 +1,27 @@
+package com.logintegra.wsbbugtracker.issues;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Issue {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column(nullable = false)
+    String title;
+
+    @Column(columnDefinition = "TEXT")
+    String content;
+
+}

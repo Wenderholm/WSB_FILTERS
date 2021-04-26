20b4dd75493274146f21aa5d193c3e31081912b9
Katarzyna Duchowska <k.duchowska@o2.pl>
2021-04-26 23:16:00 +0200

    przykłady do wykładu

--- src/main/java/com/logintegra/wsbbugtracker/MainController.java
+++ src/main/java/com/logintegra/wsbbugtracker/MainController.java
@@ -1,14 +1,43 @@
 package com.logintegra.wsbbugtracker;
 
+import com.logintegra.wsbbugtracker.issues.Issue;
+import com.logintegra.wsbbugtracker.issues.IssueQueryExample;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    final IssueQueryExample issueQueryExample;
+
+    public MainController(IssueQueryExample issueQueryExample) {
+        this.issueQueryExample = issueQueryExample;
+    }
+
     @GetMapping("/")
     public String index() {
         return "redirect:/issue";
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<Issue> all() {
+        return issueQueryExample.findAll();
+    }
+
+    @GetMapping("/todo")
+    @ResponseBody
+    public List<Issue> todo() {
+        return issueQueryExample.findAllToDo();
+    }
+
+    @GetMapping("/todoABC")
+    @ResponseBody
+    public List<Issue> todoABC() {
+        return issueQueryExample.findAllToDoAndProjectIsABC();
     }
 
 }
